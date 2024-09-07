36f52d85b9596fd4e628697239d75a3dfa29689a
Divyanshu <divyanshuvishwakarma222@gmail.com>
2024-09-07 21:17:48 +0530

    📱 Responsive : Work on homepage hero and stats section responsiveness

--- src/components/Homepage/Hero.tsx
+++ src/components/Homepage/Hero.tsx
@@ -3,14 +3,14 @@ import { icons } from "../ui/assets";
 export default function HeroSection() {
   return (
     <div className=" w-full min-h-[calc(100vh-64px)] flex items-center justify-center p-4 bg-[#003145] ">
-      <div className="max-w-[1280px] w-full flex flex-col md:flex-row items-center justify-around">
+      <div className="max-w-[1280px] w-full flex flex-row items-center justify-around">
         <div className="flex justify-start items-start gap-8 w-full h-full ">
-          <div className="w-3 h-[124px] bg-[#FFCE5C]  hidden md:block place-self-start "></div>
-          <div className="text-white max-w-xl w-full flex flex-col justify-between items-center md:items-start gap-4 h-full ">
-            <h1 className="text-3xl md:text-5xl font-semibold mb-4 w-full text-nowrap leading-loose  ">
+          <div className="w-3 h-[124px] bg-[#FFCE5C] place-self-start "></div>
+          <div className="text-white max-w-xl w-full flex flex-col justify-between items-start gap-4 h-full ">
+            <h1 className="text-5xl font-semibold mb-4 w-full leading-tight line-clamp-3  ">
               Accelerate Innovation <br /> with Global AI Challenges
             </h1>
-            <p className="text-gray-300 mb-6 leading-relaxed ">
+            <p className="text-gray-300 max-w-[530px] mb-6 leading-relaxed text-wrap line-clamp-3 ">
               AI Challenges at DPhi simulate real-world problems. It is a great
               place to put your AI/Data Science skills to test on diverse
               datasets allowing you to foster learning through competitions.
@@ -20,8 +20,8 @@ export default function HeroSection() {
             </button>
           </div>
         </div>
-        <div className="w-full max-w-[200px] md:max-w-[400px]">
-          <img src={icons.rocket} alt="rocketimg" className=" w-[400px] h-full " />
+        <div className=" hidden lg:block ">
+          <img src={icons.rocket} alt="rocketimg" className=" " />
         </div>
       </div>
     </div>
